73e8b8968efecd6a27ac9c1f90dc7cbadb0e8073
Johngridnex <ochowupunathanieljohn@gmail.com>
2026-02-10 06:39:20 +0100

    feat: integrate db connection and dotenv

--- server/index.js
+++ server/index.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config();
+
+// Connect to database
+connectDB();
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -9,5 +18,5 @@ app.get('/api', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
